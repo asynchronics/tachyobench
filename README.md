baa519ca0000db7829bfb34d09d69486ab02c25d
Serge Barral <serge.barral@asynchronics.com>
2022-10-16 16:49:23 +0200

    Initial commit as standalone benchmark

---
+++ README.md
@@ -0,0 +1,144 @@
+# The tachyonix benchmark
+
+This is a small benchmarking suite for `async` MPSC message passing, used to
+monitor regressions in [Asynchronix][asynchronix] and [Tachyonix][tachyonix].
+
+
+## Channels and runtimes
+
+At the moment, the following MPMC/MPSC channels are available:
+- [tachyonix]
+- [async-channel]
+- [flume]
+- [postage::mpsc]
+- [tokio::mpsc]
+
+It is possible to select one of the following runtimes:
+- [asynchronix]
+- [tokio]
+- [async-std]
+- [smolscale]
+
+[tachyonix]: https://github.com/asynchronics/tachyonix
+[async-channel]: https://github.com/smol-rs/async-channel
+[flume]: https://github.com/zesterer/flume
+[postage::mpsc]: https://github.com/austinjones/postage-rs
+[tokio::mpsc]: https://github.com/tokio-rs/tokio
+[asynchronix]: https://github.com/asynchronics/asynchronix
+[tokio]: https://github.com/tokio-rs/tokio
+[async-std]: https://github.com/async-rs/async-std
+[smolscale]: https://github.com/geph-official/smolscale
+
+
+## Benchmarks
+
+There are currently 2 parametric benchmarks:
+- *pinball*: fully connected graph where messages ("balls") perform a random
+  walk between nodes ("pins"),
+- *funnel*: many-to-one messaging in a tight loop.
+
+Benchmarks always run on all available logical threads.
+
+
+### Disclaimer
+
+Benchmarking multithreaded `async` code is tricky: a lot depends on the detailed
+implementation and scheduling strategy of the executor, and performance
+variability is typically much higher than with synchronous code, in particular
+when the load is not sufficient to keep all worker threads busy.
+
+*Criterion*, the de-facto standard Rust benchmarking framework, cannot be used
+as it lacks support for task spawning and multithreading. This bench uses
+straightforward execution time measurement and statistical analysis, running
+long-lasting tests to reduce noise and minimize the impact of executor startup.
+Async executors are complex enough to prevent obvious benchmark-defeating
+compiler optimizations, so no black-boxing is implemented.
+
+
+### Pinball
+
+This benchmark is an upgraded version of the classical ping-pong benchmark. Its
+main goal is to measure performance in situations where receivers are often
+starved but sender are never blocked.
+
+Each test rig consists of a complete graph (a.k.a. fully connected graph) which
+edges are the channels. Each node forwards any message it receives to another
+randomly chosen node. For this benchmark, each graph contains 13 nodes, each
+node containing in turn 1 receiver and 12 senders (1 for each other node).
+Importantly, each channel has enough capacity to never block on sending. The
+benchmark concurrently runs 61 such rigs of 13 nodes.
+
+The test is performed for various numbers of messages ("balls"), which are
+initially fairly distributed across the graph. The messages then perform a
+random walk between the nodes ("pins") until they have visited a pre-defined
+amount of nodes.
+
+
+### Funnel
+
+This benchmark is ubiquitous and often simply referred to as the "MPSC
+benchmark". It consists of a single receiver connected to many senders which
+receive and send messages in a tight loop.
+
+What this benchmark measures is very dependent on many details such as the
+relative timing of enqueue, dequeue and notification operations. Unsurprisingly,
+the standard deviation on the results is large compared to the pinball
+benchmark. Corollary: this type of benchmark is neither very realistic nor very
+objective.
+
+In this particular implementation, each receiver is connected to 13 senders. The
+benchmark runs 61 such rigs of 13 senders and 1 receiver concurrently.
+
+The test is performed for various channel capacities. Note that unlike the other
+channels, tokio's MPSC channel reserves 1 additional slot for each of the 13
+senders on top of the nominal capacity.
+
+
+## Example usage
+
+For help, type:
+
+```
+$ bench -h
+```
+
+To see all benchmarks for all channels, type:
+
+```
+$ bench -l
+```
+
+To run the *pinball* benchmark for all channels using Tokio, type:
+
+```
+$ bench pinball
+```
+
+To run all benchmarks for `tachonix` using Tokio, type:
+
+```
+$ bench tachyonix
+```
+
+To run only the *funnel* benchmark for `flume` using Tokio and average the
+results over 5 runs, type:
+
+```
+$ bench -s 5 funnel-flume
+```
+
+To run all benchmarks for `async-channel` with Asynchronix instead of Tokio, type:
+
+```
+$ bench async_channel -e asynchronix
+```
+
+## License
+
+The code in this repository is licensed under the [Apache License, Version
+2.0](LICENSE-APACHE) or the [MIT license](LICENSE-MIT), at your option. Note,
+however, that the Tokio and Smolscale dependencies are respectively available
+under the MIT license and the ISC license only. Although the ISC license is
+broadly considered compatible with the MIT license, it is your responsibility to
+ensure that any distribution by you of this bench in an executable form indeed
+conforms to the licenses of these dependencies.
---
+++ src/main.rs
@@ -0,0 +1,329 @@
+use std::collections::BTreeMap;
+use std::ffi::OsString;
+use std::fs::File;
+use std::io::Write;
+use std::num::NonZeroU32;
+
+use lexopt::prelude::*;
+
+mod benches;
+mod channel_shims;
+mod executor_shims;
+mod macros;
+
+const HELP_MESSAGE: &str = "\
+bench
+Benchmark runner for Tachyonix
+
+USAGE:
+    tachyobench [OPTIONS] <BENCHNAME>
+
+ARGS:
+    <BENCHNAME>    If specified, only run benches containing this string in their names
+
+OPTIONS:
+    -h, --help             Print help information
+    -l, --list             List available benches
+    -s, --samples SAMPLES  Repeat benches SAMPLES times and average the result
+    -o, --output FILE      Save the results to FILE
+    -e, --exec EXECUTOR    Run the bench with the EXECUTOR runtime;
+                           possible values: tokio [default], async-std,
+                           smolscale, asynchronix";
+
+macro_rules! add_test {
+    ($group:ident, $channel:ident) => {
+        (
+            stringify!($group),
+            stringify!($channel),
+            &[
+                (
+                    ExecutorId::Tokio,
+                    benches::$group::$channel::bench::<crate::executor_shims::TokioExecutor>,
+                ),
+                (
+                    ExecutorId::AsyncStd,
+                    benches::$group::$channel::bench::<crate::executor_shims::AsyncStdExecutor>,
+                ),
+                (
+                    ExecutorId::SmolScale,
+                    benches::$group::$channel::bench::<crate::executor_shims::SmolScaleExecutor>,
+                ),
+                (
+                    ExecutorId::Asynchronix,
+                    benches::$group::$channel::bench::<crate::executor_shims::AsynchronixExecutor>,
+                ),
+            ],
+        )
+    };
+}
+
+#[allow(clippy::type_complexity)]
+const BENCHES: &[(&str, &str, &[(ExecutorId, fn(NonZeroU32) -> BenchIterator)])] = &[
+    add_test!(funnel, async_channel),
+    add_test!(funnel, flume),
+    add_test!(funnel, tachyonix),
+    add_test!(funnel, postage_mpsc),
+    add_test!(funnel, tokio_mpsc),
+    add_test!(pinball, async_channel),
+    add_test!(pinball, flume),
+    add_test!(pinball, tachyonix),
+    add_test!(pinball, postage_mpsc),
+    add_test!(pinball, tokio_mpsc),
+];
+
+pub struct BenchResult {
+    label: String,
+    parameter: String,
+    throughput: Vec<f64>,
+}
+impl BenchResult {
+    pub fn new(label: String, parameter: String, throughput: Vec<f64>) -> Self {
+        Self {
+            label,
+            parameter,
+            throughput,
+        }
+    }
+}
+
+type BenchIterator = Box<dyn Iterator<Item = BenchResult>>;
+
+#[derive(PartialEq)]
+enum ExecutorId {
+    Tokio,
+    AsyncStd,
+    SmolScale,
+    Asynchronix,
+}
+impl ExecutorId {
+    const TOKIO: &str = "tokio";
+    const ASYNC_STD: &str = "async-std";
+    const SMOLSCALE: &str = "smolscale";
+    const ASYNCHRONIX: &str = "asynchronix";
+
+    fn new(name: &str) -> Result<Self, ()> {
+        match name {
+            Self::TOKIO => Ok(ExecutorId::Tokio),
+            Self::ASYNC_STD => Ok(ExecutorId::AsyncStd),
+            Self::SMOLSCALE => Ok(ExecutorId::SmolScale),
+            Self::ASYNCHRONIX => Ok(ExecutorId::Asynchronix),
+            _ => Err(()),
+        }
+    }
+    fn name(&self) -> &'static str {
+        match self {
+            ExecutorId::Tokio => Self::TOKIO,
+            ExecutorId::AsyncStd => Self::ASYNC_STD,
+            ExecutorId::SmolScale => Self::SMOLSCALE,
+            ExecutorId::Asynchronix => Self::ASYNCHRONIX,
+        }
+    }
+}
+
+struct BenchArgs {
+    bench_substrings: Vec<String>,
+    executor: ExecutorId,
+    samples: NonZeroU32,
+    output: Option<OsString>,
+}
+
+fn parse_args() -> Result<Option<BenchArgs>, lexopt::Error> {
+    let mut samples = NonZeroU32::new(1).unwrap();
+    let mut executor = ExecutorId::Tokio;
+    let mut bench_substrings = Vec::new();
+    let mut output = None;
+
+    let mut parser = lexopt::Parser::from_env();
+    while let Some(arg) = parser.next()? {
+        match arg {
+            Short('h') | Long("help") => {
+                println!("{}", HELP_MESSAGE);
+
+                return Ok(None);
+            }
+            Short('l') | Long("list") => {
+                for (group, item, _) in BENCHES {
+                    println!("    {}-{}", group, item)
+                }
+
+                return Ok(None);
+            }
+            Short('s') | Long("samples") => {
+                samples = parser.value()?.parse()?;
+            }
+            Short('o') | Long("output") => {
+                output = Some(parser.value()?);
+            }
+            Short('e') | Long("exec") => {
+                let val = parser.value()?;
+                executor = ExecutorId::new(val.clone().into_string()?.as_ref()).map_err(|_| {
+                    lexopt::Error::UnexpectedValue {
+                        option: "exec".into(),
+                        value: val,
+                    }
+                })?;
+            }
+            Value(val) => {
+                bench_substrings.push(val.into_string()?);
+            }
+            _ => return Err(arg.unexpected()),
+        }
+    }
+
+    Ok(Some(BenchArgs {
+        bench_substrings,
+        executor,
+        samples,
+        output,
+    }))
+}
+
+fn main() -> Result<(), lexopt::Error> {
+    #[allow(clippy::type_complexity)]
+    let mut benches: BTreeMap<
+        &'static str,
+        BTreeMap<&'static str, fn(NonZeroU32) -> Box<dyn Iterator<Item = BenchResult>>>,
+    > = BTreeMap::new();
+
+    let BenchArgs {
+        bench_substrings,
+        executor,
+        samples,
+        output,
+    } = match parse_args()? {
+        None => return Ok(()),
+        Some(args) => args,
+    };
+
+    // Select all requested benches.
+    for (group, item, executor_benches) in BENCHES {
+        let bench_name = format!("{}-{}", group, item);
+        if bench_substrings.is_empty()
+            || bench_substrings
+                .iter()
+                .any(|name| bench_name.contains(name))
+        {
+            let bench = executor_benches
+                .iter()
+                .find(|(id, _)| executor == *id)
+                .unwrap()
+                .1;
+            benches
+                .entry(*group)
+                .or_insert(BTreeMap::new())
+                .insert(*item, bench);
+        }
+    }
+
+    if benches.is_empty() {
+        println!("No matching benches found");
+
+        return Ok(());
+    }
+
+    // Open the result file if requested.
+    let mut output = output
+        .map(|filename| {
+            File::create(filename.clone())
+                .map_err(|_| format!("Could not open file <{}>", filename.to_str().unwrap()))
+        })
+        .transpose()?;
+
+    // Run sequentially all requested benchmarks.
+    for (group, benches) in benches {
+        println!(
+            "Running benchmark '{}' with the {} runtime.",
+            group,
+            executor.name()
+        );
+        if samples.get() != 1 {
+            println!("All results are averaged over {} runs.", samples);
+        }
+
+        // Only used when saving to file.
+        let mut column_headers = Vec::new();
+        let mut parameter_column = Vec::new();
+        let mut columns = Vec::new();
+
+        for (bench_id, (name, bench)) in benches.into_iter().enumerate() {
+            println!("    {}:", name);
+            let mut data_column = Vec::new();
+
+            for (
+                parameter_id,
+                BenchResult {
+                    label,
+                    parameter,
+                    throughput,
+                },
+            ) in bench(samples).into_iter().enumerate()
+            {
+                assert!(!throughput.is_empty());
+
+                let mean = throughput.iter().fold(0f64, |acc, s| acc + s) / throughput.len() as f64;
+
+                if output.is_some() {
+                    if bench_id == 0 && parameter_id == 0 {
+                        column_headers.push(label.clone());
+                    }
+                    if bench_id == 0 {
+                        parameter_column.push(parameter.clone());
+                    }
+                    data_column.push(format!("{:.0}", mean));
+                }
+
+                if throughput.len() == 1 {
+                    println!(
+                        "        {:<20} {:>12.3} msg/µs",
+                        format!("{}={}", label, parameter),
+                        mean / 1e6
+                    );
+                } else {
+                    let std_dev = (throughput
+                        .iter()
+                        .fold(0f64, |acc, s| acc + (s - mean) * (s - mean))
+                        / throughput.len() as f64)
+                        .sqrt();
+
+                    println!(
+                        "        {:<20} {:>12.3} msg/µs [±{:.3}]",
+                        format!("{}: {}", label, parameter),
+                        mean * 1e-6,
+                        std_dev * 1e-6
+                    );
+                }
+            }
+            if output.is_some() {
+                columns.push(data_column);
+                column_headers.push(String::from(name));
+            }
+            println!();
+        }
+
+        // Save to file if requested.
+        if let Some(file) = &mut output {
+            columns.insert(0, parameter_column);
+            writeln!(
+                file,
+                "# '{}' benchmark with {} runtime",
+                group,
+                executor.name()
+            )
+            .unwrap();
+            write!(file, "#").unwrap();
+            for header in column_headers {
+                write!(file, "{:>15} ", header).unwrap();
+            }
+            writeln!(file).unwrap();
+            for row in 0..columns[0].len() {
+                for column in &columns {
+                    write!(file, " {:>15}", column[row]).unwrap();
+                }
+                writeln!(file).unwrap();
+            }
+            writeln!(file).unwrap();
+        }
+    }
+
+    Ok(())
+}
